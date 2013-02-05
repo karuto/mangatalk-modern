2ed48cf24412955bc2efc6eca96cf092317c3b6c
Karuto Z <iamkaruto@gmail.com>
2013-02-04 23:38:09 -0800

    Start messing around with header. Make it fixed

--- header.php
+++ header.php
@@ -31,27 +31,54 @@
   <?php wp_head(); ?>
 </head>
 
+
 <body <?php body_class(); ?>>
+
 <div id="page" class="hfeed layout-page-wrap">
   <div class="layout-header-wrap">
+
     <header id="masthead" class="site-header" role="banner">
+
       <hgroup>
-        <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+        <h1 class="site-title">
+          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" 
+            title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+            <?php bloginfo( 'name' ); ?>
+          </a>
+        </h1>
         <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
       </hgroup>
 
+
       <nav id="site-navigation" class="main-navigation" role="navigation">
+
         <h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
-        <a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
+        <a class="assistive-text" href="#content" 
+          title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>">
+          <?php _e( 'Skip to content', 'twentytwelve' ); ?>
+        </a>
+
         <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+
       </nav><!-- #site-navigation -->
+
 
       <?php $header_image = get_header_image();
       if ( ! empty( $header_image ) ) : ?>
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( $header_image ); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" /></a>
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+          <img src="<?php echo esc_url( $header_image ); ?>" class="header-image" 
+          width="<?php echo get_custom_header()->width; ?>" 
+          height="<?php echo get_custom_header()->height; ?>" alt="" />
+        </a>
       <?php endif; ?>
+
+
     </header><!-- #masthead -->
+
+
   </div><!-- .layout-header-wrap -->
+
+
 
   <div id="main" class="layout-main-wrap">
     <div class="layout-main-inner-wrap">
