33da660eeb75dccd1fa92f9ae51be9b3b0002a5e
Karuto Z <iamkaruto@gmail.com>
2013-02-05 00:58:18 -0800

    Start messing with single post page

--- single.php
+++ single.php
@@ -9,25 +9,25 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
+  <div id="primary" class="layout-primary-wrap">
+    <div id="content" class="layout-content-wrap" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+      <?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', get_post_format() ); ?>
+        <?php get_template_part( 'content', get_post_format() ); ?>
 
-				<nav class="nav-single">
-					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
-					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
-					<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
-				</nav><!-- .nav-single -->
+        <nav class="nav-single">
+          <h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
+          <span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
+          <span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
+        </nav><!-- .nav-single -->
 
-				<?php comments_template( '', true ); ?>
+        <?php comments_template( '', true ); ?>
 
-			<?php endwhile; // end of the loop. ?>
+      <?php endwhile; // end of the loop. ?>
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+    </div><!-- #layout-content-wrap -->
+  </div><!-- #layout-primary-wrap -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
