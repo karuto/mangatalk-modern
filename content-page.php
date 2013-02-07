8399e6775e1a4ebd2b48fb523b0f2c1880c02922
Karuto Z <iamkaruto@gmail.com>
2013-02-06 20:15:58 -0800

    Finished basic page markup (only two column ver.)

--- content-page.php
+++ content-page.php
@@ -8,16 +8,22 @@
  */
 ?>
 
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header class="entry-header">
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-		</header>
+  <div class="layout-regular-post">
+    <article id="post-<?php the_ID(); ?>" 
+      <?php post_class('main-item-block main-item-style'); ?>>
+      
+      <header class="entry-header">
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+      </header>
 
-		<div class="entry-content">
-			<?php the_content(); ?>
-			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
-		</div><!-- .entry-content -->
-		<footer class="entry-meta">
-			<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
-		</footer><!-- .entry-meta -->
-	</article><!-- #post -->
+      <div class="entry-content">
+        <?php the_content(); ?>
+        <?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
+      </div><!-- .entry-content -->
+      <footer class="entry-meta">
+        <?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
+      </footer><!-- .entry-meta -->
+
+    </article><!-- #post -->
+  </div><!-- .layout-regular-post -->
+
--- page.php
+++ page.php
@@ -14,16 +14,23 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', 'page' ); ?>
-				<?php comments_template( '', true ); ?>
-			<?php endwhile; // end of the loop. ?>
+  <div id="primary" class="layout-primary-wrap">
+    <div id="content" class="layout-content-wrap" role="main">
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+      <div class="layout-posts-section">
+        <div class="layout-posts-row">
+
+          <?php while ( have_posts() ) : the_post(); ?>
+            <?php get_template_part( 'content', 'page' ); ?>
+            <?php comments_template( '', true ); ?>
+          <?php endwhile; // end of the loop. ?>
+        
+        </div><!-- .layout-post-row -->
+      </div><!-- .layout-posts-section -->  
+
+    </div><!-- .layout-content-wrap -->
+  </div><!-- .layout-primary-wrap -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
