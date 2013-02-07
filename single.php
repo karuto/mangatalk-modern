0c0c8286d2a12b33eed1d0018e9fb287416facf7
Karuto Z <iamkaruto@gmail.com>
2013-02-06 19:06:25 -0800

    Different column setup for feature / regular post

--- single.php
+++ single.php
@@ -12,16 +12,29 @@ get_header(); ?>
   <div id="primary" class="layout-primary-wrap">
     <div id="content" class="layout-content-wrap" role="main">
 
-      <div class="layout-posts-section">
-        <div class="layout-posts-row">
+      <?php $format = get_post_format();
+
+        // TODO: Stop using 'aside' format as featured post! Use meta box instead.
+        if ( $format == 'aside' )
+          // If it's a featured post, we print nothing to keep it a single column
+          echo '';
+        else 
+          // For regular posts, we print out the CSS table & row for two columns
+          echo '<div class="layout-posts-section">';
+          echo '<div class="layout-posts-row">';
+
+      ?>
+
           <?php while ( have_posts() ) : the_post(); ?>
 
             <?php get_template_part( 'content', get_post_format() ); ?>
 
             <nav class="nav-single">
               <h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
-              <span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
-              <span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
+              <span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . 
+              _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
+              <span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . 
+              _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
             </nav><!-- .nav-single -->
 
             <?php comments_template( '', true ); ?>
