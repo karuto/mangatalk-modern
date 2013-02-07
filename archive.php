7cdfcb5b27a68f1fb2d5e70eccd76d584766c23d
Karuto Z <iamkaruto@gmail.com>
2013-02-06 19:25:38 -0800

    Finished basic archive markup, move on baby

--- archive.php
+++ archive.php
@@ -19,45 +19,64 @@
 
 get_header(); ?>
 
-	<section id="primary" class="site-content">
-		<div id="content" role="main">
+  <div id="primary" class="layout-primary-wrap">
+    <div id="content" class="layout-content-wrap" role="main">
 
-		<?php if ( have_posts() ) : ?>
-			<header class="archive-header">
-				<h1 class="archive-title"><?php
-					if ( is_day() ) :
-						printf( __( 'Daily Archives: %s', 'twentytwelve' ), '<span>' . get_the_date() . '</span>' );
-					elseif ( is_month() ) :
-						printf( __( 'Monthly Archives: %s', 'twentytwelve' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'twentytwelve' ) ) . '</span>' );
-					elseif ( is_year() ) :
-						printf( __( 'Yearly Archives: %s', 'twentytwelve' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'twentytwelve' ) ) . '</span>' );
-					else :
-						_e( 'Archives', 'twentytwelve' );
-					endif;
-				?></h1>
-			</header><!-- .archive-header -->
+    <?php if ( have_posts() ) : ?>
+      <header class="archive-header">
+        <h1 class="archive-title"><?php
+          if ( is_day() ) :
+            printf( __( 'Daily Archives: %s', 'twentytwelve' ), '<span>' . get_the_date() . '</span>' );
+          elseif ( is_month() ) :
+            printf( __( 'Monthly Archives: %s', 'twentytwelve' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'twentytwelve' ) ) . '</span>' );
+          elseif ( is_year() ) :
+            printf( __( 'Yearly Archives: %s', 'twentytwelve' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'twentytwelve' ) ) . '</span>' );
+          else :
+            _e( 'Archives', 'twentytwelve' );
+          endif;
+        ?></h1>
+      </header><!-- .archive-header -->
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
 
-				/* Include the post format-specific template for the content. If you want to
-				 * this in a child theme then include a file called called content-___.php
-				 * (where ___ is the post format) and that will be used instead.
-				 */
-				get_template_part( 'content', get_post_format() );
+      <div class="layout-posts-section">
 
-			endwhile;
+      <?php /* Start the Loop */ 
+      $counter = 1; /* Loop counter */ 
+      echo '<div class="layout-posts-row">'; /* Print the first div row */ ?>
 
-			twentytwelve_content_nav( 'nav-below' );
-			?>
+      <?php while ( have_posts() ) : the_post(); ?>
 
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
-		<?php endif; ?>
+        <?php 
+        /* It loads content.php for normal single posts without formats */
+        get_template_part('postitem');  
 
-		</div><!-- #content -->
-	</section><!-- #primary -->
+        if ($counter % 2 == 0) {
+          /* For every 2 post items, we print a div row! */
+          echo '</div><div class="layout-posts-row">';
+        } else {
+          echo '<div class="main-item-gap"></div>';
+        }
+
+        $counter++; /* Increment counter */ 
+        ?>
+
+
+      <?php endwhile; 
+      $counter = 1; /* Make sure counter is reset */
+      echo '</div>'; /* Make sure the open div is closed */ ?>
+
+      <?php twentytwelve_content_nav( 'nav-below' ); ?>
+
+      </div><!-- .layout-posts-section -->
+
+    <?php else : ?>
+
+      <?php get_template_part( 'content', 'none' ); ?>
+      
+    <?php endif; ?>
+
+    </div><!-- .layout-content-wrap -->
+  </div><!-- .layout-primary-wrap -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
