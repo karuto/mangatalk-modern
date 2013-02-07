7eb4a97d69d256ba658af1d2ccf2f5df4b45f150
Karuto Z <iamkaruto@gmail.com>
2013-02-06 19:18:34 -0800

    Applied basic block markup to comments

--- comments.php
+++ comments.php
@@ -18,43 +18,45 @@
  * return early without loading the comments.
  */
 if ( post_password_required() )
-	return;
+  return;
 ?>
 
-<div id="comments" class="layout-comments-area">
+<div class="layout-regular-post">
+  <div id="comments" class="layout-comments-area main-item-block main-item-style ">
 
-	<?php // You can start editing here -- including this comment! ?>
+    <?php // You can start editing here -- including this comment! ?>
 
-	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'twentytwelve' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
-			?>
-		</h2>
+    <?php if ( have_comments() ) : ?>
+      <h2 class="comments-title">
+        <?php
+          printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'twentytwelve' ),
+            number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+        ?>
+      </h2>
 
-		<ol class="commentlist">
-			<?php wp_list_comments( array( 'callback' => 'twentytwelve_comment', 'style' => 'ol' ) ); ?>
-		</ol><!-- .commentlist -->
+      <ol class="commentlist">
+        <?php wp_list_comments( array( 'callback' => 'twentytwelve_comment', 'style' => 'ol' ) ); ?>
+      </ol><!-- .commentlist -->
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-below" class="navigation" role="navigation">
-			<h1 class="assistive-text section-heading"><?php _e( 'Comment navigation', 'twentytwelve' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'twentytwelve' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'twentytwelve' ) ); ?></div>
-		</nav>
-		<?php endif; // check for comment navigation ?>
+      <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+      <nav id="comment-nav-below" class="navigation" role="navigation">
+        <h1 class="assistive-text section-heading"><?php _e( 'Comment navigation', 'twentytwelve' ); ?></h1>
+        <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'twentytwelve' ) ); ?></div>
+        <div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'twentytwelve' ) ); ?></div>
+      </nav>
+      <?php endif; // check for comment navigation ?>
 
-		<?php
-		/* If there are no comments and comments are closed, let's leave a note.
-		 * But we only want the note on posts and pages that had comments in the first place.
-		 */
-		if ( ! comments_open() && get_comments_number() ) : ?>
-		<p class="nocomments"><?php _e( 'Comments are closed.' , 'twentytwelve' ); ?></p>
-		<?php endif; ?>
+      <?php
+      /* If there are no comments and comments are closed, let's leave a note.
+       * But we only want the note on posts and pages that had comments in the first place.
+       */
+      if ( ! comments_open() && get_comments_number() ) : ?>
+      <p class="nocomments"><?php _e( 'Comments are closed.' , 'twentytwelve' ); ?></p>
+      <?php endif; ?>
 
-	<?php endif; // have_comments() ?>
+    <?php endif; // have_comments() ?>
 
-	<?php comment_form(); ?>
+    <?php comment_form(); ?>
 
-</div><!-- #comments .comments-area -->
+  </div><!-- #comments .layout-comments-area -->
+</div><!-- .layout-regular-post -->
