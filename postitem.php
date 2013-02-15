c500ea824cd148c000a314260eb3186ace59036e
Karuto Z <iamkaruto@gmail.com>
2013-02-15 01:21:36 -0800

    Styling post item author and feature masthead

--- postitem.php
+++ postitem.php
@@ -10,7 +10,7 @@
 ?>
 
   <article id="post-<?php the_ID(); ?>" 
-    <?php post_class('main-item-block main-item-style'); ?>>
+    <?php post_class('main-item-block main-item-style post-item'); ?>>
 
 
 <!-- 
@@ -34,7 +34,14 @@
 
     <!-- /* Else create a div and set it as background down below */ -->
     <div class="entry-masthead" 
-    style="background-image: url('<?php echo $image[0]; ?>')"></div>
+    style="background-image: url('<?php echo $image[0]; ?>')">
+
+      <div class="author-avatar">
+        <?php echo get_avatar( get_the_author_meta( 'user_email' ), 
+        apply_filters( 'twentytwelve_author_bio_avatar_size', 75 ) ); ?>
+      </div><!-- .author-avatar -->
+
+    </div>
 
 
     <header class="entry-header">
@@ -46,13 +53,17 @@
           <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
         </h1>
       <?php endif; // is_single() ?>
-<!-- 
-      <?php if ( comments_open() ) : ?>
-        <div class="comments-link">
-          <?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentytwelve' ) . '</span>', __( '1 Reply', 'twentytwelve' ), __( '% Replies', 'twentytwelve' ) ); ?>
-        </div>
-      <?php endif; // comments_open() ?>
- -->
+
+<!--TODO
+      <div class="author-link">
+        BY 
+        <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
+          <?php echo get_the_author(); ?>
+        </a>
+      </div><!-- .author-link -->
+      <h3 class="entry-meta roboto-font">
+        By KARUTO on 2013/01/31
+      </h3><!-- .entry-postitem-meta -->
 
     </header><!-- .entry-header -->
 
@@ -72,33 +83,6 @@
       </div><!-- .entry-summary -->
 
     <?php endif; ?>
-
-
-    <footer class="entry-meta">
-
-      <?php twentytwelve_entry_meta(); ?>
-      <?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
-
-      <?php if ( is_singular() && get_the_author_meta( 'description' ) && is_multi_author() ) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries. ?>
-
-        <div class="author-info">
-          <div class="author-avatar">
-            <?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentytwelve_author_bio_avatar_size', 68 ) ); ?>
-          </div><!-- .author-avatar -->
-
-          <div class="author-description">
-            <h2><?php printf( __( 'About %s', 'twentytwelve' ), get_the_author() ); ?></h2>
-            <p><?php the_author_meta( 'description' ); ?></p>
-            <div class="author-link">
-              <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-                <?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'twentytwelve' ), get_the_author() ); ?>
-              </a>
-            </div><!-- .author-link -->
-          </div><!-- .author-description -->
-        </div><!-- .author-info -->
-      <?php endif; ?>
-
-    </footer><!-- .entry-meta -->
 
 
   </article><!-- #post -->
