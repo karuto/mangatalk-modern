99c59dccef636a52a46105370082324bcb401c31
Karuto Z <iamkaruto@gmail.com>
2013-02-16 01:17:59 -0800

    Initial styling for JS dropdown

--- mangatalk-dropdown.php
+++ mangatalk-dropdown.php
@@ -1,18 +1,18 @@
-<ul class="dropdown">
-	<li><a href="#">MangaTalk</a>
-		<ul class="sub_menu">
-             <li><a href="#">文集 Everything</a></li>
-             <li><a href="#">专题 Features</a></li>
-             <li><a href="#">漫评 Reviews</a></li>
-             <li><a href="#">资讯 News</a></li>
-             <li><a href="#">访谈 Interviews</a></li>
-			 <li>
-				<a href="#">Batting Cages</a>
-				<ul>
-					<li><a href="http://google.com">Indoor</a></li>
-					<li><a href="#">Outdoor</a></li>
-				</ul>
-			 </li>
+<ul class="global-dropdown">
+	<li class="touch-logo"><a href="#">MangaTalk</a>
+		<ul class="drop-list">
+      <li class="drop-item"><a href="#">文集 Everything</a></li>
+      <li class="drop-item"><a href="#">专题 Features</a></li>
+      <li class="drop-item"><a href="#">漫评 Reviews</a></li>
+      <li class="drop-item"><a href="#">资讯 News</a></li>
+      <li class="drop-item"><a href="#">访谈 Interviews</a></li>
+      <li class="drop-item">
+        <a href="#">Batting Cages</a>
+        <ul>
+          <li><a href="http://google.com">Indoor</a></li>
+          <li><a href="#">Outdoor</a></li>
+        </ul>
+      </li>
 		</ul>
 	</li>
 </ul>
