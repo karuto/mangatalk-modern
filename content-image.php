8c1f1b7684df0edec014a0504b8802c2a0aeaade
Karuto Z <iamkaruto@gmail.com>
2013-02-17 13:45:21 -0800

    Converted image posts' indentation and UTF

--- content-image.php
+++ content-image.php
@@ -25,29 +25,29 @@ echo wp_get_attachment_image_src( get_post_thumbnail_id(), 'medium')[0] . "<br>"
 $attachments = get_posts($args);
 if ($attachments) {
     foreach ($attachments as $attachment) {
-    	// Returns a full URI for an attachment file or false on failure
-		echo wp_get_attachment_image_src( $attachment->ID, 'medium')[0] . "<br>";
-	}
+      // Returns a full URI for an attachment file or false on failure
+    echo wp_get_attachment_image_src( $attachment->ID, 'medium')[0] . "<br>";
+  }
 }
 
 ?>
 
 
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<div class="entry-content">
-			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
-		</div><!-- .entry-content -->
+  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="entry-content">
+      <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
+    </div><!-- .entry-content -->
 
-		<footer class="entry-meta">
-			<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark">
-				<h1><?php the_title(); ?></h1>
-				<h2><time class="entry-date" datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>"><?php echo get_the_date(); ?></time></h2>
-			</a>
-			<?php if ( comments_open() ) : ?>
-			<div class="comments-link">
-				<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentytwelve' ) . '</span>', __( '1 Reply', 'twentytwelve' ), __( '% Replies', 'twentytwelve' ) ); ?>
-			</div><!-- .comments-link -->
-			<?php endif; // comments_open() ?>
-			<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
-		</footer><!-- .entry-meta -->
-	</article><!-- #post -->
+    <footer class="entry-meta">
+      <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark">
+        <h1><?php the_title(); ?></h1>
+        <h2><time class="entry-date" datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>"><?php echo get_the_date(); ?></time></h2>
+      </a>
+      <?php if ( comments_open() ) : ?>
+      <div class="comments-link">
+        <?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentytwelve' ) . '</span>', __( '1 Reply', 'twentytwelve' ), __( '% Replies', 'twentytwelve' ) ); ?>
+      </div><!-- .comments-link -->
+      <?php endif; // comments_open() ?>
+      <?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
+    </footer><!-- .entry-meta -->
+  </article><!-- #post -->
