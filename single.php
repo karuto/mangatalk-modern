9edf83f5e040b3620aa983b9659ad8ee7252a05c
Karuto Z <iamkaruto@gmail.com>
2013-02-17 21:04:13 -0800

    Applied styles for single post nav (prev / next)

--- single.php
+++ single.php
@@ -36,13 +36,13 @@ get_header(); ?>
         <?php /* This is where the meat comes in */
           get_template_part( 'content', get_post_format() ); ?>
 
-        <nav class="nav-single">
-          <h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
+        <nav class="entry-nav-single main-item-style">
+          <!-- <h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3> -->
           <span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . 
           _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
           <span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . 
           _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
-        </nav><!-- .nav-single -->
+        </nav><!-- .entry-nav-single -->
 
         <?php comments_template( '', true ); ?>
 
