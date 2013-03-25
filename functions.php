e92b76ab6d3f3d254d378d3684ff2d3b17e2d333
Karuto Z <iamkaruto@gmail.com>
2013-03-25 16:48:59 -0700

    Coding up page navigation

--- functions.php
+++ functions.php
@@ -247,12 +247,23 @@ function twentytwelve_content_nav( $html_id ) {
 
   if ( $wp_query->max_num_pages > 1 ) : ?>
 
-    <nav id="<?php echo $html_id; ?>" class="navigation" role="navigation">
+    <nav id="<?php echo $html_id; ?>" class="layout-page-navigation" role="navigation">
       <?php echo paginate_links( $args ) ?>
-      <h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
-      <div class="nav-previous alignleft"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentytwelve' ) ); ?></div>
-      <div class="nav-next alignright"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?></div>
-    </nav><!-- #<?php echo $html_id; ?> .navigation -->
+      <!-- <h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3> -->
+      <div class="layout-posts-row">
+        <div class="main-item-block main-item-style post-item nav-item nav-previous">
+          <div class="entry-summary">
+            <?php next_posts_link('« 更旧的文章 | OLDER', 0); ?>
+          </div>
+        </div>
+        <div class="main-item-gap">
+        <div class="main-item-block main-item-style post-item nav-item nav-next">
+          <div class="entry-summary">
+            <?php previous_posts_link('NEWER | 更新的文章 »', 0); ?>
+          </div>          
+        </div>
+      </div>
+    </nav><!-- #<?php echo $html_id; ?> .page-navigation -->
   <?php endif;
 }
 endif;
