f296bb044874d4f44c4bf73c95d3ff0ac4512824
Karuto Z <iamkaruto@gmail.com>
2013-03-28 16:47:25 -0700

    Replaced site logo with an image

--- mangatalk-dropdown.php
+++ mangatalk-dropdown.php
@@ -1,9 +1,11 @@
 <ul class="global-dropdown">
 	<li class="touch-logo">
-      <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" 
+      <h1>
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" 
       title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-      <!-- <span class="sitename-cn">漫言 </span> -->MangaTalk 
-      <span class="site-desc">漫言</span></a></h1>
+        <img src="<?php echo get_template_directory_uri(); ?>/logo.png" alt="漫言 MangaTalk">
+        </a>
+      </h1>
 		<ul class="drop-list">
       <li class="drop-item"><a href="#">文集 Everything</a></li>
       <li class="drop-item"><a href="#">专题 Features</a></li>
