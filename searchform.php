23d6aa90d206bf481b077e2852772a4716e9c66e
Karuto Z <iamkaruto@gmail.com>
2013-03-31 03:43:39 -0700

    Styling search form

--- searchform.php
+++ searchform.php
@@ -9,8 +9,28 @@
  */
 ?>
 
+<style type="text/css">
+#searchform {
+  display: inline;
+}
+#searchform input {
+  font-size: 12px;
+}
+#searchform-text {
+  border: 0px;
+  border-bottom: 1px solid #d0d0d0;
+}
+#searchform-submit {
+  background: transparent url(<?php echo get_template_directory_uri(); ?>/searchicon.png) no-repeat;
+  border: 0;
+  height: 30px;
+  padding: 0px;
+  text-indent: -9999px;
+  width: 30px;
+}
+</style>
 
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-  <input type="text" value="" name="s" id="s" />
-  <input type="submit" id="searchsubmit" value="Search" />
+  <input id="searchform-text" type="text" value="" name="s" id="s" />
+  <input id="searchform-submit" type="submit" id="searchsubmit" value="Search" />
 </form>
