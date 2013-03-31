d17096b9404b1d76386e275c066a183c56d02fc8
Karuto Z <iamkaruto@gmail.com>
2013-03-31 02:52:56 -0700

    Cleaned up post item, simplified logic

--- postitem.php
+++ postitem.php
@@ -12,16 +12,6 @@
   <article id="post-<?php the_ID(); ?>" 
     <?php post_class('main-item-block main-item-style post-item'); ?>>
 
-
-<!-- 
-     <?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
-      <div class="featured-post">
-        <?php _e( 'Featured post', 'twentytwelve' ); ?>
-      </div>
-    <?php endif; ?> 
- -->
-
-
     <?php 
     /* Retrieving the featured image URL of this post */
     $image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'large')[0]; ?>
@@ -47,13 +37,6 @@
         </h1>
       <?php endif; // is_single() ?>
 
-<!--TODO
-      <div class="author-link">
-        BY 
-        <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-          <?php echo get_the_author(); ?>
-        </a>
-      </div><!-- .author-link -->
       <h3 class="entry-meta roboto-font">
         By 
         <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author"><?php the_author() ?></a>
@@ -62,26 +45,8 @@
 
     </header><!-- .entry-header -->
 
-
-    <?php if ( is_search() ) : // Only display full content for search results
-    // TODO: change this when implementing actual search ?>
-
-      <div class="entry-content">
-        <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
-        <?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
-      </div><!-- .entry-content -->
-
-    <?php else : // Only display excerpts for regular pages ?>    
-
-      <div class="entry-summary">
-        <?php the_excerpt(); ?>
-      </div><!-- .entry-summary -->
-
-<!--       <div class="entry-readmore">
-        <span class="readmore roboto">MORE</span>
-      </div> -->
-
-    <?php endif; ?>
-
+    <div class="entry-summary">
+      <?php the_excerpt(); ?>
+    </div><!-- .entry-summary -->
 
   </article><!-- #post -->
