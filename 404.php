36cb8ac4f2c91c543b5613b7311347e985d8ecaa
Karuto Z <iamkaruto@gmail.com>
2013-04-01 18:36:24 -0700

    Started fixing 404 page template

--- 404.php
+++ 404.php
@@ -8,22 +8,33 @@
  */
 
 get_header(); ?>
+  <div class="layout-subheader-wrap">
+    <header class="sub-header" roll="banner">
 
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
+      <h1 class="archive-title sub-title">
+        很抱歉，您所查找的内容并不存在。 
+      </h1>
+    </header><!-- .sub-header -->
+  </div><!-- .layout-subheader-wrap -->
 
-			<article id="post-0" class="post error404 no-results not-found">
-				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentytwelve' ); ?></h1>
-				</header>
 
-				<div class="entry-content">
-					<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'twentytwelve' ); ?></p>
-					<?php get_search_form(); ?>
-				</div><!-- .entry-content -->
-			</article><!-- #post-0 -->
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+
+  <div id="primary" class="site-content">
+    <div id="content" role="main">
+
+      <article id="post-0" class="post error404 no-results not-found">
+        <header class="entry-header">
+          <h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentytwelve' ); ?></h1>
+        </header>
+
+        <div class="entry-content">
+          <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'twentytwelve' ); ?></p>
+          <?php get_search_form(); ?>
+        </div><!-- .entry-content -->
+      </article><!-- #post-0 -->
+
+    </div><!-- #content -->
+  </div><!-- #primary -->
 
 <?php get_footer(); ?>
