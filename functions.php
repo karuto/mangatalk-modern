d9f3d8d6888d8e03f2fa83de4de8f8cc3200e2e7
Karuto Z <iamkaruto@gmail.com>
2013-04-02 23:30:32 -0700

    Removed legacy image caption filter code that is no longer supported

--- functions.php
+++ functions.php
@@ -478,35 +478,6 @@ add_action( 'customize_preview_init', 'twentytwelve_customize_preview_js' );
  */
 
 
-
-
-/**
- * Author: Karuto
- *
- * Fix the system-enforced in-line style, extra 10 pixel width issue for image captions.
- * @since Twenty Twelve 1.0
- */
-add_shortcode('wp_caption', 'fixed_img_caption_shortcode');
-add_shortcode('caption', 'fixed_img_caption_shortcode');
-function fixed_img_caption_shortcode($attr, $content = null) {
-  // Allow plugins/themes to override the default caption template.
-  $output = apply_filters('img_caption_shortcode', '', $attr, $content);
-  if ( $output != '' ) return $output;
-  extract(shortcode_atts(array(
-    'id'=> '',
-    'align' => 'alignnone',
-    'width' => '',
-    'caption' => ''), $attr));
-  if ( 1 > (int) $width || empty($caption) )
-  return $content;
-  if ( $id ) $id = 'id="' . esc_attr($id) . '" ';
-  return '<div ' . $id . 'class="wp-caption ' . esc_attr($align)
-  . '" style="width: ' . ((int) $width) . 'px">'
-  . do_shortcode( $content ) . '<p class="wp-caption-text">'
-  . $caption . '</p></div>';
-}
-
-
 /**
  * Author: Karuto
  *
