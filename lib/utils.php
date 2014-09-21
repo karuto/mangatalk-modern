f0e7da4d0215bebdef94c6f0398029def02d4630
Karuto Z <iamkaruto@gmail.com>
2014-09-20 19:25:53 -0700

    Imported new starter theme for foundation

---
+++ lib/utils.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Utility functions
+ */
+function is_element_empty($element) {
+  $element = trim($element);
+  return !empty($element);
+}
+
+// Tell WordPress to use searchform.php from the templates/ directory
+function roots_get_search_form($form) {
+  $form = '';
+  locate_template('/templates/searchform.php', true, false);
+  return $form;
+}
+add_filter('get_search_form', 'roots_get_search_form');
