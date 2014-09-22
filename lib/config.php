d403dd35d05d2053da946aeda80a94395995f3d9
Karuto Z <iamkaruto@gmail.com>
2014-09-21 23:50:25 -0700

    Added config

--- lib/config.php
+++ lib/config.php
@@ -62,7 +62,7 @@ function roots_display_sidebar() {
      * Any of these page templates that return true won't show the sidebar
      */
     array(
-      'template-custom.php'
+      'template-custom.php' 
     )
   );
 
