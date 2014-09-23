659093b61a92b0234b3ecdc35c3352a34e9619a2
Karuto Z <iamkaruto@gmail.com>
2014-09-22 17:32:35 -0700

    Added logic to render different layout alternative rows

--- archive.php
+++ archive.php
@@ -8,16 +8,25 @@
 <?php endif; ?>
 
 <div class="mt-list">
-<?php $count = 1; ?>
+<?php $count = 1; $rows = 0; ?>
 <?php while (have_posts()) : the_post(); ?>
   <?php 
-  if ($count % 2 != 0) { // odd, render the start of the row
+  if ($count % 2 != 0) { // odd post, render the start of the row
     echo '<div class="row">';
-    echo '<section class="mt-block col-xs-12 col-sm-6 col-md-8">'; 
+    $rows++;
+    if ($rows % 2 != 0) { // odd row
+      echo '<section class="mt-block col-xs-12 col-sm-6 col-md-8">'; 
+    } else { // even row
+      echo '<section class="mt-block col-xs-12 col-sm-6 col-md-4">'; 
+    }
     get_template_part('templates/content', get_post_format()); 
     echo '</section>';
-  } else { // even, render the end of the row
-    echo '<section class="mt-block col-xs-12 col-sm-6 col-md-4">';   
+  } else { // even post, render the end of the row
+    if ($rows % 2 != 0) { // odd row
+      echo '<section class="mt-block col-xs-12 col-sm-6 col-md-4">'; 
+    } else { // even row
+      echo '<section class="mt-block col-xs-12 col-sm-6 col-md-8">'; 
+    }  
     get_template_part('templates/content', get_post_format()); 
     echo '</section>';
     echo '</div>'; 
