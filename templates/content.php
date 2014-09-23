acaa3681249ad69f58ff7e08629e63b54b3aa46a
Karuto Z <iamkaruto@gmail.com>
2014-09-22 17:06:33 -0700

    Debugging color overlay for blocks on list page

--- templates/content.php
+++ templates/content.php
@@ -1,17 +1,28 @@
-<article class="block-cover" <?php post_class();
-   // Retrieve cover image URL then set header's background
-  if ( has_post_thumbnail() ) {
-    $cover_img_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' )[0];
-  } else {
-    $cover_img_url = 'http://i.imgur.com/zk0ecX8.jpg';
-  }
-  echo 'style="background-image: url(' . $cover_img_url . ');"';
-  ?>>
-  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php get_template_part('templates/entry-meta'); ?>
-  </header>
-  <div class="entry-summary">
-    <?php the_excerpt(); ?>
-  </div>
+<article class="block-panel">
+  <div class="block-cover-wrapper">
+    
+    <div class="block-cover overlay" <?php post_class();
+     // Retrieve cover image URL then set header's background
+    if ( has_post_thumbnail() ) {
+      $cover_img_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' )[0];
+    } else {
+      $cover_img_url = 'http://i.imgur.com/zk0ecX8.jpg';
+    }
+    echo 'style="background-image: url(' . $cover_img_url . ');"';
+    ?>>
+    
+    </div>
+    
+    <div class="row block-content">
+      <header>
+        <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <?php get_template_part('templates/entry-meta'); ?>
+      </header>
+    
+      <div class="entry-summary">
+        <?php the_excerpt(); ?>
+      </div>
+    </div>
+    
+  </div><!-- .block-cover-wrapper -->
 </article>
