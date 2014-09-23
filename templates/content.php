6a5907ef8ec18b812575847529147b4360d73784
Karuto Z <iamkaruto@gmail.com>
2014-09-22 17:24:29 -0700

    Implemented overlay sucessfully

--- templates/content.php
+++ templates/content.php
@@ -11,18 +11,21 @@
     echo 'style="background-image: url(' . $cover_img_url . ');"';
     ?>>
     
-    </div>
-    
-    <div class="row block-content">
-      <header>
-        <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <?php get_template_part('templates/entry-meta'); ?>
-      </header>
-    
-      <div class="entry-summary">
-        <?php the_excerpt(); ?>
-      </div>
-    </div>
+      <div class="row block-content">
+        
+        <header class="block-header">
+          
+          <h2 class="entry-title text-contrast"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <?php get_template_part('templates/entry-meta'); ?>
+          <div class="entry-summary text-contrast">
+            <?php the_excerpt(); ?>
+          </div>
+          
+        </header>
+        
+      </div><!-- .block-content -->
+      
+    </div><!-- .block-cover -->
     
   </div><!-- .block-cover-wrapper -->
 </article>
