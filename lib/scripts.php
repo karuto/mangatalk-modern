ae9d86f9806e8d4fefa725b4136d0e80ab5506f7
Karuto Z <iamkaruto@gmail.com>
2014-09-23 06:12:57 -0700

    Inited homepage which will only be used for displaying front page nothing else

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '71d94b58ee4f61530c465b0885dbd2ad');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c8b82a10763461c0836ec8a9f9125219');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
