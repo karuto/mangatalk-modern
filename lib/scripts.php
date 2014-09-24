fd2e4fcc0b4ef00e7b2ea0dc64e4f7117d81f8fc
Karuto Z <iamkaruto@gmail.com>
2014-09-24 02:30:50 -0700

    Adjusted CHN type family, detail fix

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '590982ce7d8d1f0c50656edeeae9ce44');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '84df4ab8a5f2998d1098d98b6cb7903a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
