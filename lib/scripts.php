282e10dbf3c6c88b454d641250cbd37458245337
Karuto Z <iamkaruto@gmail.com>
2014-09-25 03:20:35 -0700

    Completed author and post meta

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4489fec0df2309612af8fe90e26ad397');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9b960abe0c408fc5fc6dbe00f28a2bae');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/content-single.php
+++ templates/content-single.php
@@ -86,16 +86,23 @@
   <div id="author-info" class="author-info clearfix">  
     <header class="meta-header author-info-header"> 本文作者&emsp;|&emsp;Author Information</header>
     <div class="row">
-      <div class="avatar">
-        <?php echo get_avatar( get_the_author_meta( 'user_email' ), 80); ?>
+      <div class="avatar col-xs-3 col-md-3">
+        <?php echo get_avatar( get_the_author_meta( 'user_email' ), 150); ?>
       </div>
-      <div class="author-name">
-        <?php echo get_the_author_link(); ?>  发表于 <?php echo get_the_time('Y/m/d g:i:s A'); ?>
+      <div class="col-xs-9 col-md-9">
+        <div class="author-name">
+          <?php echo get_the_author_link(); ?>
+        </div>
+        <p class="article-meta-spec">
+           Written on <?php echo get_the_date(); ?>. 
+           <?php the_tags( '<span>Tagged with ', ' | ', '</span>' ); ?>
+        </p>
+        <p class="author-bio">
+          <?php echo get_the_author_meta('description', $post->post_author ); ?> 
+        </p>
       </div>
-      <p class="author-bio">
-        <?php echo get_the_author_meta('description', $post->post_author ); ?> 
-      </p>
-    </div>
+
+    </div><!-- .row -->
   
   </div>
     
