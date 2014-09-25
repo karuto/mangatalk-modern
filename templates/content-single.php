640579bd713f62b19acfab20a91a561a989d6386
Karuto Z <iamkaruto@gmail.com>
2014-09-25 02:58:29 -0700

    Added markup for author header, ready to add row & col

--- templates/content-single.php
+++ templates/content-single.php
@@ -60,15 +60,15 @@
         if ( $related_posts_query->have_posts() ) { ?>
 
   <div id="related-article-list" class="related-article-list clearfix">  
-  <header class="meta-header related-article-header">联动阅读&emsp;|&emsp;Further Readings</header>
+    <header class="meta-header related-article-header">联动阅读&emsp;|&emsp;Further Readings</header>
   
   <?php   while( $related_posts_query->have_posts() ) {  
             $related_posts_query->the_post(); ?> 
       
-  <section class="mt-block clearfix">
-    <?php get_template_part('templates/content', get_post_format()); ?>
-  </section>
-
+      <section class="mt-block clearfix">
+        <?php get_template_part('templates/content', get_post_format()); ?>
+      </section>
+      
   <?php   } // endwhile; ?> 
   
   </div><!-- .related-article-list -->
@@ -82,19 +82,23 @@
       wp_reset_query();
   ?>  
   
+
+  <div id="author-info" class="author-info clearfix">  
+    <header class="meta-header author-info-header"> 本文作者&emsp;|&emsp;Author Information</header>
+    <div class="row">
+      <div class="avatar">
+        <?php echo get_avatar( get_the_author_meta( 'user_email' ), 80); ?>
+      </div>
+      <div class="author-name">
+        <?php echo get_the_author_link(); ?>  发表于 <?php echo get_the_time('Y/m/d g:i:s A'); ?>
+      </div>
+      <p class="author-bio">
+        <?php echo get_the_author_meta('description', $post->post_author ); ?> 
+      </p>
+    </div>
   
-  <div class="author-container">
-    <div class="avatar">
-      <?php echo get_avatar( get_the_author_meta( 'user_email' ), 80); ?>
-    </div>
-    <div class="author-name">
-      <?php echo get_the_author_link(); ?>  发表于 <?php echo get_the_time('Y/m/d g:i:s A'); ?>
-    </div>
-    <p class="author-bio">
-      <?php echo get_the_author_meta('description', $post->post_author ); ?> 
-    </p>
-    
   </div>
+    
 </div>
 
 
