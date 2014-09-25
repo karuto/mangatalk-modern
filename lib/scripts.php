3def7620e4e564d52a6e119a46cf0bdc0840d165
Karuto Z <iamkaruto@gmail.com>
2014-09-24 17:16:32 -0700

    Added related posts by tag -WIP-, and type details refinement

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fa3694596336ff6bacd2c78875444d1b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b7b3d92e2edc21068ccf1e8dfac2aa9d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/content-single.php
+++ templates/content-single.php
@@ -44,6 +44,42 @@
 <div class="entry-meta-container article-content-container">
   <div class="divider"></div>
   
+  <div class="relatedposts">  
+  <h3>Related posts</h3>  
+  <?php  
+      $orig_post = $post;  
+      global $post;  
+      $tags = wp_get_post_tags($post->ID);  
+      
+      if ($tags) {  
+      $tag_ids = array();  
+      foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;  
+      $args=array(  
+      'tag__in' => $tag_ids,  
+      'post__not_in' => array($post->ID),  
+      'posts_per_page'=>4, // Number of related posts to display.  
+      'caller_get_posts'=>1  
+      );  
+      
+      $my_query = new wp_query( $args );  
+  
+      while( $my_query->have_posts() ) {  
+      $my_query->the_post();  
+      ?>  
+      
+      <div class="relatedthumb">  
+          <a rel="external" href="<? the_permalink()?>"><?php the_post_thumbnail(array(150,100)); ?><br />  
+          <?php the_title(); ?>  
+          </a>  
+      </div>  
+      
+      <? }  
+      }  
+      $post = $orig_post;  
+      wp_reset_query();  
+      ?>  
+  </div>
+  
   <div class="author-container">
     <div class="avatar">
       <?php echo get_avatar( get_the_author_meta( 'user_email' ), 80); ?>
