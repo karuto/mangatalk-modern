375af933bc7125eff894dec2603aba7e79dc8713
Karuto Z <iamkaruto@gmail.com>
2014-09-26 14:40:49 -0700

    Modularized author info meta

--- templates/content-single-image.php
+++ templates/content-single-image.php
@@ -7,31 +7,8 @@
 
 <div class="entry-meta-container article-content-container">
 
-  <?php get_template_part('templates/content', 'relatedposts'); ?>
-
-  <div id="author-info" class="author-info clearfix">  
-    <header class="meta-header author-info-header"> 本文作者&emsp;|&emsp;Author Information</header>
-    <div class="row">
-      <div class="avatar col-xs-3 col-md-3">
-        <?php echo get_avatar( get_the_author_meta( 'user_email' ), 150); ?>
-      </div>
-      <div class="col-xs-9 col-md-9">
-        <div class="author-name">
-          <?php echo get_the_author_link(); ?>
-        </div>
-        <p class="article-meta-spec">
-           Written on <?php echo get_the_date(); ?>. 
-           <?php the_tags( '<span>Published under: ', ' | ', '</span>' ); ?>
-        </p>
-        <p class="author-bio">
-          <?php echo get_the_author_meta('description', $post->post_author ); ?> 
-        </p>
-      </div>
-
-    </div><!-- .row -->
-  
-  </div><!-- .author-info -->
-  
+  <?php get_template_part('templates/meta', 'relatedposts'); ?>
+  <?php get_template_part('templates/meta', 'authorinfo'); ?>
   
   <div class="comments-root-container clearfix">
     <header class="meta-header author-info-header"> 留言讨论&emsp;|&emsp;Goings-On About Town</header>
--- templates/content-single.php
+++ templates/content-single.php
@@ -53,30 +53,10 @@
 
 <div class="entry-meta-container article-content-container">
   
-  <?php get_template_part('templates/content', 'relatedposts'); ?>
+  <?php get_template_part('templates/meta', 'relatedposts'); ?>
+  <?php get_template_part('templates/meta', 'authorinfo'); ?>
 
-  <div id="author-info" class="author-info clearfix">  
-    <header class="meta-header author-info-header"> 本文作者&emsp;|&emsp;Author Information</header>
-    <div class="row">
-      <div class="avatar col-xs-3 col-md-3">
-        <?php echo get_avatar( get_the_author_meta( 'user_email' ), 150); ?>
-      </div>
-      <div class="col-xs-9 col-md-9">
-        <div class="author-name">
-          <?php echo get_the_author_link(); ?>
-        </div>
-        <p class="article-meta-spec">
-           Written on <?php echo get_the_date(); ?>. 
-           <?php the_tags( '<span>Tags: ', ' | ', '</span>' ); ?>
-        </p>
-        <p class="author-bio">
-          <?php echo get_the_author_meta('description', $post->post_author ); ?> 
-        </p>
-      </div>
-
-    </div><!-- .row -->
   
-  </div><!-- .author-info -->
   
   
   <div class="comments-root-container clearfix">
