b3c9795c811dad2ac3d518b0daee546376e63e37
Karuto Z <iamkaruto@gmail.com>
2014-09-26 04:11:22 -0700

    Modularized related posts

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c3fb82986ac5b5fdd7156b14482dee7d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c36a40aef3f4200f8d6e725231c274ea');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/content-single.php
+++ templates/content-single.php
@@ -52,46 +52,8 @@
 </div>
 
 <div class="entry-meta-container article-content-container">
-  <?php  
-      $orig_post = $post; // store variable
-      global $post;  
-      $tags = wp_get_post_tags($post->ID);
-      if ($tags) {  
-        $tag_ids = array();  
-        foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;  
-        $args=array(  
-          'tag__in' => $tag_ids,  
-          'post__not_in' => array($post->ID),  
-          'posts_per_page'=>5, // Number of related posts to display.
-        );  
-      
-        $related_posts_query = new wp_query( $args );  
-        
-        if ( $related_posts_query->have_posts() ) { ?>
-
-  <div id="related-article-list" class="related-article-list clearfix">  
-    <header class="meta-header related-article-header">联动阅读&emsp;|&emsp;Further Readings</header>
   
-  <?php   while( $related_posts_query->have_posts() ) {  
-            $related_posts_query->the_post(); ?> 
-      
-      <section class="mt-block mini-block clearfix">
-        <?php get_template_part('templates/content', get_post_format()); ?>
-      </section>
-      
-  <?php   } // endwhile; ?> 
-  
-  </div><!-- .related-article-list -->
-  
-  <?php } else { ?>
-      
-  <?php 
-        } // endif have_posts();
-      } // endif $tags;
-      $post = $orig_post; // restore variables
-      wp_reset_query();
-  ?>  
-  
+  <?php get_template_part('templates/content', 'relatedposts'); ?>
 
   <div id="author-info" class="author-info clearfix">  
     <header class="meta-header author-info-header"> 本文作者&emsp;|&emsp;Author Information</header>
