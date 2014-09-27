d1830ab2dce74331b6a56ad56ec0108646814d75
Karuto Z <iamkaruto@gmail.com>
2014-09-27 06:55:01 -0700

    Added regular and fullwidth page templates

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8a254065aa3dd2ecc900f3e8a182afd9');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c4c83379d98069f1b7c6f0dc3710bef7');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
