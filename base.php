939d1a59cb368e6efdff195166eb798424d6c903
Karuto Z <iamkaruto@gmail.com>
2014-09-27 22:24:58 -0700

    Disabled whole site for public view, admin only

--- base.php
+++ base.php
@@ -1,6 +1,8 @@
 <?php get_template_part('templates/head'); ?>
 <body <?php body_class(); ?>>
-
+<?php
+if ( is_user_logged_in() ) { ?>
+  
   <!--[if lt IE 8]>
     <div class="alert alert-warning">
       <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
@@ -34,6 +36,14 @@
   </div><!-- /.wrap -->
 
   <?php get_template_part('templates/footer'); ?>
+  
+<?php
+} else {
+  get_template_part('404');
+}
+?>  
+
+
 
 </body>
 </html>
