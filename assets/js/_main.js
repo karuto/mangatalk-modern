eafc8b755d61f5d9b169efa8367f5d2e0b19e570
Karuto Z <iamkaruto@gmail.com>
2014-09-28 07:15:57 -0700

    Added JS for tooltip

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -23,7 +23,7 @@ var Roots = {
   common: {
     init: function() {
       // JavaScript to be fired on all pages
-      $('.entry-content').tooltip();
+      $('[data-toggle="tooltip"]').tooltip();
     }
   },
   // Home page
