a40f0e68a7b469100906d5027a1c7e4bbb9baded
Karuto Z <iamkaruto@gmail.com>
2014-09-28 20:41:56 -0700

    Fixed mobile styles, footer links

--- templates/meta-authorinfo.php
+++ templates/meta-authorinfo.php
@@ -1,10 +1,10 @@
   <div id="author-info" class="author-info clearfix">  
     <header class="meta-header author-info-header"> 本文作者&emsp;|&emsp;Author of Honor</header>
     <div class="row">
-      <div class="avatar col-xs-3 col-md-3">
+      <div class="avatar col-xs-12 col-md-3">
         <?php echo get_avatar( get_the_author_meta( 'user_email' ), 150); ?>
       </div>
-      <div class="col-xs-9 col-md-9">
+      <div class="col-xs-12 col-md-9">
         <div class="author-name">
           <?php echo get_the_author_link(); ?>
         </div>
