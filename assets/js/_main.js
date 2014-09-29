a3947e7f3394b2fcc3a8742a18ba2d1ccc52bd1e
Karuto Z <iamkaruto@gmail.com>
2014-09-29 03:43:47 -0700

    Fixed contact form, ditched mail now using db to store

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -281,31 +281,31 @@ if (cbSlides.length != 0) {
 
 var contactForm = $('form#contactForm');
 if (contactForm.length > 0) {
-	$('form#contactForm').submit(function() {
+	contactForm.submit(function() {
 		$('form#contactForm .error').remove();
 		var hasError = false;
 		$('.requiredField').each(function() {
 			if(jQuery.trim($(this).val()) == '') {
 				var labelText = $(this).prev('label').text();
-				$(this).parent().append('<span class="error">You forgot to enter your '+labelText+'.</span>');
+				$(this).parent().append('<label class="error">请输入您的'+labelText+'。</label>');
 				hasError = true;
 			} else if($(this).hasClass('email')) {
 				var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 				if(!emailReg.test(jQuery.trim($(this).val()))) {
 					var labelText = $(this).prev('label').text();
-					$(this).parent().append('<span class="error">You entered an invalid '+labelText+'.</span>');
+					$(this).parent().append('<label class="error">您输入的'+labelText+'格式不正确。</label>');
 					hasError = true;
 				}
 			}
 		});
 		if(!hasError) {
-			$('form#contactForm li.buttons button').fadeOut('normal', function() {
+			$('form#contactForm button').fadeOut('normal', function() {
 				$(this).parent().append('<img src="/wp-content/themes/td-v3/images/template/loading.gif" alt="Loading&hellip;" height="31" width="31" />');
 			});
 			var formInput = $(this).serialize();
 			$.post($(this).attr('action'),formInput, function(data){
-				$('form#contactForm').slideUp("fast", function() {				   
-					$(this).before('<p class="thanks"><strong>Thanks!</strong> Your email was successfully sent. I check my email all the time, so I should be in touch soon.</p>');
+				contactForm.slideUp("fast", function() {				   
+					$(this).before('<div class="thanks"><h3>感谢你的联络。</h3><p>我们会抽空仔细阅读你的留言，谢谢！</p></div>');
 				});
 			});
 		}
