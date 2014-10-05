17f3bd68f2ead5dd5b6324d4f3d39968abd58354
Karuto Z <iamkaruto@gmail.com>
2014-10-05 01:46:37 -0700

    Added subscribe and question block

--- base-post.php
+++ base-post.php
@@ -27,6 +27,25 @@
     </div><!-- /.content -->
   </div><!-- /.wrap -->      
   
+  <div class="article-content-container">
+  <section class="promo-section row">
+    
+    <div class="promo-block col-lg-6">
+      <header class="promo-header">
+        <a href="http://goo.gl/R2TOHA" target="_blank">填写调查问卷</a>
+      </header>
+      <span class="promo-text text-muted">您的反馈，对我们意义重大。</span>
+    </div>
+    <div class="promo-block col-lg-6">
+      <header class="promo-header">
+        <a href="http://goo.gl/LVj65m" target="_blank">订阅漫言漫报</a>
+      </header>
+      <span class="promo-text text-muted">每季文摘，以及最亲密联络。</span>
+    </div>
+    
+  </section>
+  </div>
+  
   <?php get_template_part('templates/footer'); ?>
   
 </body>
