e16980049ab52d4ac227a4e049281dcc0d0b3cde
Karuto Z <iamkaruto@gmail.com>
2014-10-04 17:15:41 -0700

    Added en content

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '452d94c6bb811fca1e43e11cc198295e');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'af5cede0e418a89aed217b5867c1b78b');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/content-page-about.php
+++ templates/content-page-about.php
@@ -56,12 +56,35 @@
   <div class="featurette-inner container">
     <div class="row">
       <div class="featurette-content col-md-6">
-        <h2 class="featurette-heading">精益求精。</h2>
+        <h2 class="featurette-heading">
+          <?php 
+            global $post; 
+            if ($is_chinese) {
+              echo "精益求精。";
+            } else {
+              echo "High standards";
+            }
+          ?>
+        </h2>
         <p class="lead">
-          专注业界深入解读、主打长篇完全原创。<br>
-          没有资讯快餐，没有动游音影，全心全意关注漫画。<br>
-          漫言背后严谨而专业的编辑审校机制，<br>
-          致力于将最高质量的文章，献给最懂得欣赏的你。<br>
+          <?php 
+            global $post; 
+            if ($is_chinese) {
+              echo "
+                专注业界深入解读、主打长篇完全原创。<br>
+                没有资讯快餐，没有动游音影，全心全意关注漫画。<br>
+                漫言背后严谨而专业的编辑审校机制，<br>
+                致力于将最高质量的文章，献给最懂得欣赏的你。<br>
+                ";
+            } else {
+              echo "
+                No junk news. No click-baits. No fluff.<br>
+                It's 100% comics at MangaTalk.<br>
+                A strong focus on authentic, long-form storytelling.<br>
+                ";
+            }
+          ?>
+          
           <a href="/story">您是作者？讲述你的故事</a>
         </p>
       </div>
@@ -78,13 +101,40 @@
         <img class="featurette-image alignleft img-responsive" src="http://i.imgur.com/ZfeoqfE.png" alt="尊识至源。">
       </div>
       <div class="featurette-content col-sm-12 col-md-6">
-        <h2 class="featurette-heading">尊识至源。</h2>
+        <h2 class="featurette-heading">
+          <?php 
+            global $post; 
+            if ($is_chinese) {
+              echo "
+                尊识至源。
+                ";
+            } else {
+              echo "
+                Do No Evil
+                ";
+            }
+          ?>
+        </h2>
         <p class="lead">
-          不侵权，不抄袭，还版权于知识。<br>
-          所有编译摘抄均注明来源出处，<br>
-          所有全文转载皆经过许可授权，<br>
-          我们曾与日本国家图书馆等机构进行官方合作，<br>
-          坚持以正确的方式传播漫画文化价值。<br>
+          <?php 
+            global $post; 
+            if ($is_chinese) {
+              echo "
+                不侵权，不抄袭，还版权于知识。<br>
+                所有编译摘抄均注明来源出处，<br>
+                所有全文转载皆经过许可授权，<br>
+                我们曾与日本国家图书馆等机构进行官方合作，<br>
+                坚持以正确的方式传播漫画文化价值。<br>
+                ";
+            } else {
+              echo "
+                We honor the authenticity of knowledge to the fullest.<br>
+                Plagiarism and reproduction without permission is strictly prohibited.<br>
+                We have worked with noticable industry organizations such as Japanese National Congress Library, Kodansha and Blambot Press, dedicated to spread the comics culture the right way.<br>
+                ";
+            }
+          ?>
+          
           <a href="/business">您是媒体？欢迎联络合作</a>
         </p>
       </div>
@@ -96,13 +146,41 @@
   <div class="featurette-inner container">
     <div class="row">
       <div class="featurette-content col-sm-12 col-md-6">
-        <h2 class="featurette-heading">品读深思。</h2>
+        <h2 class="featurette-heading">
+          <?php 
+            global $post; 
+            if ($is_chinese) {
+              echo "
+                品读深思。
+                ";
+            } else {
+              echo "
+                Design-driven
+                ";
+            }
+          ?>
+          
+        </h2>
         <p class="lead">
-          中文互联网上领先的排版设计。<br>
-          爱写作，但和你一样，我们更爱阅读。<br>
-          每一处留白都精心考量、每一个字节都匠心独具，<br>
-          无广告、无干扰，雕琢最优雅的沉浸式阅读体验。<br>
-          领会中文字体排印之美。<br>
+          <?php 
+            global $post; 
+            if ($is_chinese) {
+              echo "
+                中文互联网上领先的排版设计。<br>
+                爱写作，但和你一样，我们更爱阅读。<br>
+                每一处留白都精心考量、每一个字节都匠心独具，<br>
+                无广告、无干扰，雕琢最优雅的沉浸式阅读体验。<br>
+                领会中文字体排印之美。<br>
+                ";
+            } else {
+              echo "
+                Featuring leading CJK typography.<br>
+                No distractions, no ads, our team is dedicated to bring you the premium reading experience.<br>
+                From letters to bytes, we fine-tuned every visible detail, crafting a design that's lived up to the standards of our content.<br>
+                ";
+            }
+          ?>
+          
           <a href="/feedback">您是读者？写下您的建议</a>
         </p>
       </div>
