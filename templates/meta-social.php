3acfc126c749387e3920ec636c99f71c4b63ac4e
Karuto Z <iamkaruto@gmail.com>
2014-10-20 23:38:30 -0700

    Added social template

---
+++ templates/meta-social.php
@@ -0,0 +1,12 @@
+<?php
+  global $post, $posts;
+  $first_img = '';
+  ob_start();
+  ob_end_clean();
+  $output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+  $first_img = $matches[1][0];
+
+  if(empty($first_img)) {
+    $first_img = "/path/to/default.png";
+  }
+?>
