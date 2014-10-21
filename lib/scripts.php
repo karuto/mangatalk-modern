b432b73873be4b52ace8f450f007bbceca73db14
Karuto Z <iamkaruto@gmail.com>
2014-10-21 03:38:43 -0700

    Added douban link

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '41d8c75493a9ed12b32f896864b54c84');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ed91fb7b8d4a48d9ac2263ed7374a6e9');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/meta-social.php
+++ templates/meta-social.php
@@ -11,15 +11,28 @@
   }
   
   $format_title = wp_title( '|', false, 'right' );
+  $format_link = get_permalink();
+  $format_excerpt = get_the_excerpt();
   // $weibo_link = 'http://service.weibo.com/share/share.php?appkey=1';
-  $seg_post = "&url=".get_permalink();
+  $seg_post = "&url=".$format_link;
   $seg_wbid = "&ralateUid=2634774843";
   $seg_title = "&title=".$format_title;
   $seg_pic = "&pic=".$first_img;
   $weibo_link = 'http://service.weibo.com/share/share.php?appkey=1'.$seg_post.$seg_wbid.$seg_title.$seg_pic;
   // echo $weibo_link;
   
+  $douban_link = "http://www.douban.com/share/service?image=".$first_img."&href=".$format_link."&name=".$format_title."&text=".$format_excerpt;
+  
+  $twitter_link = "http://twitter.com/intent/tweet?text=".$format_title." 来自「漫言 MangaTalk」 ".$format_link;
 ?>
-<div class="social-shares">
-  <a href="<?php echo $weibo_link?>"> 分享到微博</a>
+<div class="social-shares row">
+  <div class="col-sm-4">
+    <a href="<?php echo $weibo_link; ?>"> 分享到微博</a>
+  </div>
+  <div class="col-sm-4">
+    <a href="<?php echo $douban_link; ?>"> 分享到豆瓣</a>
+  </div>
+  <div class="col-sm-4">
+    <a href="<?php echo $twitter_link; ?>"> 分享到 Twitter</a>
+  </div>
 </div>
