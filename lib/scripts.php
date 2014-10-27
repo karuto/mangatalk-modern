bcd77735bac81990fbe8acb716f3d711203c502e
Karuto Z <iamkaruto@gmail.com>
2014-10-26 22:45:18 -0700

    Reduced meta component font sizes

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '35678dd59071edf172c7bd2fe3eee1f6');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ba5405231f32f6a8a905d7b5863e1d21');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
