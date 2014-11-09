0b66696bebba27cf8656874490b5515202330f90
Karuto Z <iamkaruto@gmail.com>
2014-11-08 21:32:38 -0800

    Added author pagecheck

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,11 @@
+<?php if (is_author()): ?>
+
+<?php else: ?>
+
 <div class="page-header">
   <header class="meta-header">
     <?php echo roots_title(); ?> 文集
   </header>
 </div>
+
+<?php endif; ?>
