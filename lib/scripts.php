475724aa95bc75aee5d96995ef857fbb1bc93a70
Karuto Z <iamkaruto@gmail.com>
2014-11-14 00:27:21 -0800

    Fixed row margin thats not default 15px

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd18088c3486af1c9903795ee5de3c887');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ebfc469db5ced3b44a998d58f92b7c5e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/loop-grid.php
+++ templates/loop-grid.php
@@ -1,30 +1,15 @@
+<div class="row">
   <?php $blocks = 1; $rows = 0; ?>
   <?php 
   function outputBlock() {
-    echo '<section class="mt-block col-xs-12 col-sm-4 col-md-4">'; 
+    echo '<section class="mt-block col-xs-12 col-sm-6 col-md-4 col-lg-3">'; 
     get_template_part('templates/content', get_post_format()); 
     echo '</section>';
   }
-  
-  while (have_posts()) : the_post(); 
-  // echo "<h1>".($blocks%3)."</h1>";
-    if ($blocks % 3 != 0) { // 1st & 2nd post of the 3, render start of the row
-      if ($blocks % 3 == 1) { // 1st post of the 3
-        echo '<div class="row">';
-        $rows++;
-      }
-      outputBlock();
-    } else { // last post of the 3, render the end of the row
-      outputBlock();
-      echo '</div>';
-    }
-    $blocks++;
+
+  while (have_posts()) {
+    the_post(); 
+    outputBlock();
+  }
   ?>
-    
-  <?php endwhile; 
-  // Note: $count starts at 1 so we have to do minus 1 to check real amount
-  // Right now $count is the total amount of posts displayed
-  // if (($count-1) % 2 != 0) {// if we get a total odd number of posts, print an extra row end
-  //   echo '</div>';
-  // }
-  ?>
+</div>
