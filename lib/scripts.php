db1264bcfeecd574a85bbf0867c1a9a149157956
Karuto Z <iamkaruto@gmail.com>
2014-11-13 23:48:06 -0800

    Added 3 column

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3d485551ac1d4d5494c3d6c9be9287da');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd18088c3486af1c9903795ee5de3c887');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/loop-grid.php
+++ templates/loop-grid.php
@@ -1,34 +1,30 @@
-  <?php $count = 1; $rows = 0; ?>
-  <?php while (have_posts()) : the_post(); ?>
-    
-    <?php 
-    if ($count % 2 != 0) { // odd post, render the start of the row
-      echo '<div class="row">';
-      $rows++;
-      if ($rows % 2 != 0) { // odd row
-        echo '<section class="mt-block col-xs-12 col-sm-6 col-md-6">'; 
-      } else { // even row
-        echo '<section class="mt-block col-xs-12 col-sm-6 col-md-6">'; 
+  <?php $blocks = 1; $rows = 0; ?>
+  <?php 
+  function outputBlock() {
+    echo '<section class="mt-block col-xs-12 col-sm-4 col-md-4">'; 
+    get_template_part('templates/content', get_post_format()); 
+    echo '</section>';
+  }
+  
+  while (have_posts()) : the_post(); 
+  // echo "<h1>".($blocks%3)."</h1>";
+    if ($blocks % 3 != 0) { // 1st & 2nd post of the 3, render start of the row
+      if ($blocks % 3 == 1) { // 1st post of the 3
+        echo '<div class="row">';
+        $rows++;
       }
-      get_template_part('templates/content', get_post_format()); 
-      echo '</section>';
-    } else { // even post, render the end of the row
-      if ($rows % 2 != 0) { // odd row
-        echo '<section class="mt-block col-xs-12 col-sm-6 col-md-6">'; 
-      } else { // even row
-        echo '<section class="mt-block col-xs-12 col-sm-6 col-md-6">'; 
-      }  
-      get_template_part('templates/content', get_post_format()); 
-      echo '</section>';
-      echo '</div>'; 
+      outputBlock();
+    } else { // last post of the 3, render the end of the row
+      outputBlock();
+      echo '</div>';
     }
-    $count++;
-    ?>
+    $blocks++;
+  ?>
     
   <?php endwhile; 
   // Note: $count starts at 1 so we have to do minus 1 to check real amount
   // Right now $count is the total amount of posts displayed
-  if (($count-1) % 2 != 0) {// if we get a total odd number of posts, print an extra row end
-    echo '</div>';
-  }
+  // if (($count-1) % 2 != 0) {// if we get a total odd number of posts, print an extra row end
+  //   echo '</div>';
+  // }
   ?>
