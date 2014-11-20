1dc6e04c5e7870644119e6f10e3f7832818f63aa
Karuto Z <iamkaruto@gmail.com>
2014-11-19 23:15:12 -0800

    Added media queries for cover

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6ee24ac613104f57e1d7e3a06b80fdf8');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '472c60eb7835e41ab34e58b6a81effec');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
