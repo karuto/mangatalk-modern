2bbdbb945cd6a9da7d5d7e66d6f777df394f0fa2
Karuto Z <iamkaruto@gmail.com>
2014-11-21 02:08:51 -0800

    Fixed hover spacing change issue

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6daf50fd0ad5ee32056d32aaf8d5cddc');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7b59216f83d21ed9af2207d3221ab312');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
