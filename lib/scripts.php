5781e4084447a05f3d7663bcf76bbe41ecab40f5
Karuto Z <iamkaruto@gmail.com>
2014-11-22 03:21:35 -0800

    Made page lead same font size as content

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '959788325c6710613d00708c29a094ca');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ab965ef34f4df5199e904d505b34ffe0');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
