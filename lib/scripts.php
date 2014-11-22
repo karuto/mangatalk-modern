08cf168ab970946c6b708ec027d09619e4f15f19
Karuto Z <iamkaruto@gmail.com>
2014-11-21 18:32:15 -0800

    Added transition effect for blocks

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd9e451675dcf94cc048502a4f9069bd1');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '959788325c6710613d00708c29a094ca');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
