add7d13fef391600e004d976f5604a25e2fb2f00
Karuto Z <iamkaruto@gmail.com>
2014-11-23 23:53:39 -0800

    Fixed featurette paddings

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8de7d084699e240c978eaceb934b85ed');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4d40d99bccbe2404ceffa99ff52c01e0');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
