fe50f0f62ab8c55e0b7f6073b111aa712127c81a
Karuto <iamkaruto@gmail.com>
2015-10-07 23:19:38 -0700

    Improved logo markup

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '359822e8040c5c1361f90995105c1c9c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '436fe796fb4f6c39067e804828158e9c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
