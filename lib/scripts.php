b34b6691989626183c6386981e49ffe58b474170
Vincent Zhang <vzhang@opentable.com>
2015-10-19 09:50:11 -0700

    Unified font size for ul, ol

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c0ce030d02f758003dbcd67744cbb154');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4c324a52a845b132d82557102c234383');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
