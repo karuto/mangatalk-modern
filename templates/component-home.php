485c1c83a8d146491fd3f16d06bf329df3093a13
Vincent Zhang <vzhang@opentable.com>
2015-10-21 15:49:02 -0700

    Improved spacing among blocks

--- templates/component-home.php
+++ templates/component-home.php
@@ -2,30 +2,47 @@
   .test .mt-block {
     height: 10rem;
   }
+  .mt-block-wrapper {
+    margin-right: 15px;
+    background: white;
+    width: 100%;
+  }
 </style>
 
 <div class="test mt-list container">
   <div class="row">
-    <section class="mt-block col-sm-12 col-md-8" style="background: white;">
-      feature + newest 5
+    <section class="mt-block col-sm-12 col-md-8">
+      <div class="mt-block-wrapper" style="background: lightblue;">
+        feature + newest 5
+      </div>
     </section>
     <!-- col-sm row divider -->
-    <section class="mt-block col-sm-4" style="background: grey;">
-      feature
+    <section class="mt-block col-xs-6 col-sm-4">
+      <div class="mt-block-wrapper" style="background: lightgrey;">
+        feature + newest 5
+      </div>
     </section>
-    <section class="mt-block col-sm-8 col-md-6" style="background: lightblue;">
-      columns
-    </section>
-    <!-- col-sm row divider -->
-    <section class="mt-block col-sm-8 col-md-6" style="background: grey;">
-      columns
-    </section>
-    <section class="mt-block col-sm-4" style="background: yellow;">
-      signup
+    <section class="mt-block col-xs-6 col-sm-8 col-md-6">
+      <div class="mt-block-wrapper" style="background: lightblue;">
+        feature + newest 5
+      </div>
     </section>
     <!-- col-sm row divider -->
-    <section class="mt-block col-sm-8 col-md-8" style="background: grey;">
-      columns
+    <section class="mt-block col-xs-6 col-sm-8 col-md-6">
+      <div class="mt-block-wrapper" style="background: grey;">
+        feature + newest 5
+      </div>
+    </section>
+    <section class="mt-block col-xs-6 col-sm-4">
+      <div class="mt-block-wrapper" style="background: lightblue;">
+        feature + newest 5
+      </div>
+    </section>
+    <!-- col-sm row divider -->
+    <section class="mt-block col-xs-6 col-sm-8 col-md-8">
+      <div class="mt-block-wrapper" style="background: lightblue;">
+        feature + newest 5
+      </div>
     </section>
   </div><!-- .row -->
 </div><!-- .mt-list -->
