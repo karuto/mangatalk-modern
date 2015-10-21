e5f7c8b47cf026ca9fd92bf9d6081fd096c1f896
Vincent Zhang <vzhang@opentable.com>
2015-10-21 15:27:20 -0700

    Added basic layout

--- templates/component-home.php
+++ templates/component-home.php
@@ -1,3 +1,26 @@
-<div class="mt-list container">
-  <h1>Hi</h1>
+<style>
+  .test .mt-block {
+    height: 10rem;
+  }
+</style>
+
+<div class="test mt-list container">
+  <div class="row">
+    <section class="mt-block col-sm-12 col-md-8" style="background: white;">
+      feature + newest 5
+    </section>
+    <!-- col-sm row divider -->
+    <section class="mt-block col-sm-4" style="background: grey;">
+      feature
+    </section>
+    <section class="mt-block col-sm-8 col-md-6" style="background: lightblue;">
+      columns
+    </section>
+    <section class="mt-block col-sm-8 col-md-6" style="background: grey;">
+      columns
+    </section>
+    <section class="mt-block col-sm-4" style="background: yellow;">
+      feature
+    </section>
+  </div><!-- .row -->
 </div><!-- .mt-list -->
