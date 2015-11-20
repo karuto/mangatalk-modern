8ca15e1aafe63c33ac4bee4242aba24a85a287ec
Karuto <iamkaruto@gmail.com>
2015-11-19 23:24:22 -0800

    Fixed indentation

--- templates/head.php
+++ templates/head.php
@@ -9,16 +9,16 @@
   <?php wp_head(); ?>
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
-  
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-55145791-1', 'auto');
-  ga('send', 'pageview');
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-</script>
-<meta property="wb:webmaster" content="1f71be3e6043edc4" /><!-- for fucking Weibo -->
+    ga('create', 'UA-55145791-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
+  <meta property="wb:webmaster" content="1f71be3e6043edc4" /><!-- for fucking Weibo -->
 </head>
