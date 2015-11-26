651bfd2e149f4c6f6bcc1d88dbdbde55dc5dedd0
Karuto <iamkaruto@gmail.com>
2015-11-25 17:37:04 -0800

    Restructured CSS, added overlay for home hero cover

--- templates/home/cover.php
+++ templates/home/cover.php
@@ -1,4 +1,8 @@
-<div id="home-cover">
-  <h1>Lorem</h1>
-  <img data-src="https://i.imgur.com/OWp5YYb.jpg" />
-</div>
+<section role="main" class="hero-cover">
+  <div class="hero-cover-image">
+    <img data-src="https://i.imgur.com/OWp5YYb.jpg" />
+  </div>
+  <div class="hero-cover-text">
+    <h1>Lorem ipsum</h1>
+  </div>
+</section>
