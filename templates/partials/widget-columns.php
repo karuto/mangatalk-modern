a915de8b9ed726f29b4d2380e7d3629b6195f3fb
Vincent Zhang <iamkaruto@gmail.com>
2019-05-16 16:14:42 -0700

    Added latest post

--- templates/partials/widget-columns.php
+++ templates/partials/widget-columns.php
@@ -62,9 +62,33 @@ $users = get_users(
             <?php echo get_avatar( $user->ID, 40 ); ?>
           </div>
           <div class="columnrec__meta">
-            <a class="columnrec__author__link" href="<?php echo get_author_posts_url( $user->ID ); ?>">
-              <?php echo $user->display_name ?>
+            <a class="columnrec__authorlink" href="<?php echo get_author_posts_url( $user->ID ); ?>">
+              <?php echo $user->display_name; ?>
+              <span class="columnrec__authorcount">
+                <?php echo count_user_posts( $user->ID ); ?> 篇文章
+              </span>
             </a>
+            <div class="columnrec__desc">
+              <?php 
+              // get the latest 1 post by the author.
+              $userPosts = get_posts(
+                array(
+                  'author'  => $user->ID,
+                  'category' => 1,
+                  'orderby' => 'post_date',
+                  'order' => 'DESC',
+                  'posts_per_page' => 1
+                )
+              );
+              foreach ( $userPosts as $post ) {
+                echo '<a class="columnrec__desclink" href="';
+                echo the_permalink();
+                echo '">';
+                echo the_title();
+                echo '</a>';
+              }
+              ?>
+            </div>
           </div>
         </div>
       </div>
