e9cc51a4f368bd2fc386f796f6243733280d1318
Vincent Zhang <iamkaruto@gmail.com>
2019-05-15 17:27:12 -0700

    Import otkit icons in meta

--- templates/content/content-feed-meta.php
+++ templates/content/content-feed-meta.php
@@ -11,4 +11,10 @@
  */
 ?>
 
-<?php the_category(' &bull; '); ?>&nbsp;&bull;&nbsp;<?php get_template_part( 'templates/content/content-post-views' ); ?>
+<?php 
+
+echo '<div class="feed__meta">';
+echo get_the_author_meta( 'display_name' );
+echo '</div>';
+get_template_part( 'templates/content/content-post-views' ); 
+?>
