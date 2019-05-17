07a6fe5fc3babba3cd2c3013b3d6b963b326b141
Vincent Zhang <iamkaruto@gmail.com>
2019-05-17 10:09:13 -0700

    Added support for custom author cover

--- templates/archive.php
+++ templates/archive.php
@@ -10,7 +10,19 @@
  */
 ?>
 
-<div class="stage stage--relative">
+<?php 
+// Retrieve cover image URL then set header's background
+$cover_img_url = get_the_author_meta( 'coverimage' );
+if ( $cover_img_url == "" ) {
+  $cover_img_url = get_template_directory_uri().'/assets/img/thumb_default_lg.jpg';
+}
+// echo $cover_img_url;
+// echo 'style="background-image: url(' . $cover_img_url . ');"';
+?>
+
+<div 
+  class="stage stage--relative <?php echo is_author() ? 'stage--author' : ''; ?>" 
+  style="background-image: url(<?php echo $cover_img_url; ?>);">
   <div class="archive-head">
     <?php echo get_the_archive_title(); ?>
   </div>
