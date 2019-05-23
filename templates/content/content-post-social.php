743ddf4bf126db3ce68004e5b7ea312330c11c4d
Vincent Zhang <iamkaruto@gmail.com>
2019-05-23 15:51:19 -0700

    Add editing link if you logged in

--- templates/content/content-post-social.php
+++ templates/content/content-post-social.php
@@ -1,4 +1,5 @@
 <div class="social">
+  <span class="social__label"><?php edit_post_link(); ?></span>
   <span class="social__label">分享至：</span>
   <a class="social__link" href="<?php echo $twitter_link; ?>" target="_blank"><?php echo getSocialIcon( 'twitter' ); ?></a>
   <a class="social__link" href="<?php echo $weibo_link; ?>" target="_blank"><?php echo getSocialIcon( 'weibo' ); ?></a>
