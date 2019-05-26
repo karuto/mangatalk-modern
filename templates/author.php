7e1e4e2cb16c21725fec07567885dbacc278dd74
Vincent Zhang <iamkaruto@gmail.com>
2019-05-25 23:59:02 -0700

    Add author ID fetching

---
+++ templates/author.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#category
+ *
+ * @package WordPress
+ * @subpackage karuto_starter_theme
+ * @since 1.0.0
+ */
+?>
+
+<?php 
+$authorId = get_the_author_meta( 'ID' );
+$authorDisplayName = get_the_author_meta( 'display_name' );
+
+// Retrieve cover image URL then set header's background
+$coverImgUrl = get_the_author_meta( 'coverimage' );
+if ( $coverImgUrl == "" ) {
+  $coverImgUrl = get_template_directory_uri().'/img/mt-cover-brand.jpg';
+}
+?>
+
+<div 
+  class="stage stage--relative stage--author"  
+  style="background-image: url(<?php echo $coverImgUrl; ?>);">
+  <div class="stage-heading">
+    <?php echo $authorDisplayName ?>
+  </div>
+</div>
+<?php get_template_part( 'templates/partials/feed' ); ?>
