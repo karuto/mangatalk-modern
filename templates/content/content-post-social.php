3ee79fce4652569c818c608a37c8825cbe596dc2
Vincent Zhang <iamkaruto@gmail.com>
2019-06-03 20:06:28 -0700

    Fix edit link

--- templates/content/content-post-social.php
+++ templates/content/content-post-social.php
@@ -1,5 +1,7 @@
 <div class="social">
-  <span class="social__label"><?php edit_post_link(); ?></span>
+  <?php if( current_user_can( 'editor' ) || current_user_can( 'administrator' ) ) {  ?>
+    <span class="social__label"><?php edit_post_link(); ?></span>
+  <?php } ?>
   <span class="social__label">分享至：</span>
   <a class="social__link" href="<?php echo $twitter_link; ?>" target="_blank"><?php echo getSocialIcon( 'twitter' ); ?></a>
   <a class="social__link" href="<?php echo $weibo_link; ?>" target="_blank"><?php echo getSocialIcon( 'weibo' ); ?></a>
