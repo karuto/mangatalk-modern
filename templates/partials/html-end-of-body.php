f3924fcfe46dbe0f46c30b02130deb0ba0fb37b7
Vincent Zhang <iamkaruto@gmail.com>
2019-06-06 15:32:44 -0700

    Fix GA

--- templates/partials/html-end-of-body.php
+++ templates/partials/html-end-of-body.php
@@ -9,9 +9,15 @@
  */
 ?>
 
-<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() . '/js/app.js' ?>"/>
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-55145791-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-55145791-1');
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() . '/js/app.js' ?>">
 </script>
 <meta property="wb:webmaster" content="1f71be3e6043edc4" />
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-55145791-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-55145791-1');
+</script>
