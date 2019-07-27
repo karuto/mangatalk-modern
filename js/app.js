d5fc9b19ae8987b6112f744396a89cabc53b56c4
henix <shellpick@gmail.com>
2019-07-27 12:15:14 +0800

    detect css grid support in js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-console.log('hi');
+// Detect CSS Grid support
+// https://codepen.io/kamakalolii/pen/MpraLZ
+document.body.classList.add(
+	typeof document.createElement("div").style.grid === "string" ? "css-grid" : "no-css-grid"
+);
--- templates/partials/html-end-of-body.php
+++ templates/partials/html-end-of-body.php
@@ -9,8 +9,15 @@
  */
 ?>
 
+<script>
+// polyfills
+if (!document.body.classList) {
+	document.write('<script src="https://cdn.jsdelivr.net/npm/dom4@2.1.5/build/dom4.js"><\/script>');
+}
+</script>
 <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() . '/js/app.js' ?>">
 </script>
+
 <meta property="wb:webmaster" content="1f71be3e6043edc4" />
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-55145791-1"></script>
