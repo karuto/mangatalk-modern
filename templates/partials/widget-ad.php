607f4e6f872427349557ebae0f3fc4e612cc92b4
Vincent Zhang <iamkaruto@gmail.com>
2019-11-11 17:39:46 -0800

    Feature: replace static ad with random post

--- templates/partials/widget-ad.php
+++ templates/partials/widget-ad.php
@@ -1,11 +1,36 @@
+<?php
+  $args = array( 
+    'orderby' => 'rand',
+    'posts_per_page' => '1', 
+    'post_type' => 'post'
+  );
+  $randomPosts = new WP_Query( $args );
+  while ( $randomPosts->have_posts() ) :
+    $randomPosts->the_post();
+    $randomPostId = get_the_ID();
+
+    $imageUrl = '';
+    if ( has_post_thumbnail( $randomPostId ) ) {
+      $image = wp_get_attachment_image_src( get_post_thumbnail_id( $randomPostId ), 'large' );
+      $imageUrl = $image[0];
+    } else {
+      $imageUrl = get_template_directory_uri() . '/img/mt-cover-brand.jpg';
+    }
+?>
+
 <div class="sidebar__block">
-  <div class="heading"><span class="heading__label">独家连载</span></div>
-    <div class="special">
+  <div class="heading"><span class="heading__label">随机推荐</span></div>
+    <div class="special" style="background-image: url('<?php echo $imageUrl; ?>')">
     <a class="special__link" href="http://mangatalk.net/2012/morning-manga-lab-1/">
-      讲谈社《Morning》漫画技术论
+      <?php the_title(); ?>
       <div class="special__link__desc">
-        川口开治教你读懂史上最硬核铁血军事漫画
+      <?php echo get_the_excerpt(); ?>
       </div>
     </a>
   </div>
 </div>
+
+<?php
+endwhile;
+wp_reset_postdata();
+?>
